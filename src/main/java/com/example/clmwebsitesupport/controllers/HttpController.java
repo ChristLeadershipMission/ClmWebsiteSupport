65f9756282f9d58767e12870cde8bbd8760fbfc5
Moyinoluwa Michael <michael@openfactorgroup.com>
2024-02-09 01:41:19 +0100

    cron jobs

--- src/main/java/com/example/clmwebsitesupport/controllers/HttpController.java
+++ src/main/java/com/example/clmwebsitesupport/controllers/HttpController.java
@@ -1,9 +1,16 @@
 package com.example.clmwebsitesupport.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("clmWebsiteSupport/api/v1/http")
+@RequestMapping("/clmWebsiteSupport/api/v1/http")
 public class HttpController {
+
+    @GetMapping("log")
+    public String keepAwake(){
+        System.out.println("Waking...");
+        return "I'm awake";
+    }
 }
